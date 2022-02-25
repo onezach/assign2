4aaeea4797940832b1cf71b76edce57f40a2869d
pat-loughran <71353422+pat-loughran@users.noreply.github.com>
2022-02-24 20:27:55 -0600

    updated Switch.java

--- src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,6 +1,11 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.MACAddress;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
@@ -8,8 +13,37 @@ import edu.wisc.cs.sdn.vnet.Iface;
 /**
  * @author Aaron Gember-Jacobson
  */
+
+
+class SwitchNode {
+	private byte[] _MACaddr;
+	private Iface _interface;
+	private int secondsLeft;
+
+	public SwitchNode(byte[] _MACaddr, Iface _interface, int secondsLeft) {
+		this._MACaddr = _MACaddr;
+		this._interface = _interface;
+		this.secondsLeft = secondsLeft;
+	}
+
+	@Override
+    public boolean equals(Object o) {
+ 
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof SwitchNode)) {
+            return false;
+        }
+        SwitchNode swn = (SwitchNode) o;
+         
+        return Arrays.equals(this._MACaddr, swn._MACaddr);
+	}
+}
+
 public class Switch extends Device
 {	
+	private ArrayList<SwitchNode> switchTable; //This is the Switch Table
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -17,6 +51,7 @@ public class Switch extends Device
 	public Switch(String host, DumpFile logfile)
 	{
 		super(host,logfile);
+
 	}
 
 	/**
@@ -29,9 +64,23 @@ public class Switch extends Device
 		System.out.println("*** -> Received packet: " +
 				etherPacket.toString().replace("\n", "\n\t"));
 		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
+		MACAddress MACin = etherPacket.getSourceMAC();
+		MACAddress MACout = etherPacket.getDestinationMAC();
+		byte[] MACinBytes = MACin.toBytes();
+		byte[] MACoutBytes = MACout.toBytes();
+		SwitchNode table_node_in = new SwitchNode(MACinBytes, inIface, 15);
+
+		// check if the table is empty, if so add source address to table
+		if (!switchTable.contains(table_node_in)) 
+			switchTable.add(table_node_in);
+
+		// check if the table contains the destination adress
+		if(!switchTable.contains(new SwitchNode(MACoutBytes, null, 0))) {
+			// flood, find end host, add to table
+		}
+		// destination host is in table, send packet
+
 		
-		/********************************************************************/
+		
 	}
 }
