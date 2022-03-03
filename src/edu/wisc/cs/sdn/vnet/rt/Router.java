3010983572336e942bb45e96634d92cab837506e
onezach <87786580+onezach@users.noreply.github.com>
2022-03-03 15:00:42 -0600

    t5

--- src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -130,7 +130,11 @@ public class Router extends Device
 		// update payloud, update source and destination mac addresses, and send!
 		// etherPacket.setPayload(payload);
 		etherPacket.setSourceMACAddress(route.getInterface().getMacAddress().toBytes());
-		etherPacket.setDestinationMACAddress(arpCache.lookup(route.getDestinationAddress()).getMac().toBytes());
+		// etherPacket.setDestinationMACAddress(arpCache.lookup(route.getDestinationAddress()).getMac().toBytes());
+		if ((arpCache.lookup(route.getDestinationAddress())) == null) {
+			System.out.println("null getMac");
+		}
+		etherPacket.setDestinationMACAddress((arpCache.lookup(route.getDestinationAddress()).getMac()).toBytes());
 		this.sendPacket(etherPacket, route.getInterface());
 		/********************************************************************/
 	}
